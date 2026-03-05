c0d8e6835ef05742f4ae3572fd9a5a02b328caa0
GIT-kiran-kumar <120352944+GIT-kiran-kumar@users.noreply.github.com>
2026-03-05 16:36:46 +0530

    Add additional favicon and apple-touch-icon links

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,6 +36,9 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
